6c6f9db98c0a5188b3bbf6320725285f83e9d50a
Tera.Lin <Tera.Lin@axiomtek.com.tw>
2022-05-16 16:56:33 +0800

    Move rosbirdge connection to seperate JS file

--- Static/js/dashboard/dashboardCtrl.js
+++ Static/js/dashboard/dashboardCtrl.js
@@ -1,39 +1,4 @@
 
-console.log("DashboardCtrl loaded");
-
-    // Connecting to ROS
-    // -----------------
-    var ros = new ROSLIB.Ros();
-    
-    // If there is an error on the backend, an 'error' emit will be emitted.
-    ros.on('error', function(error) {
-      console.log('Web Socket Connection error: '+error);
-    });
-    
-    // Find out exactly when we made a connection.
-    ros.on('connection', function() {
-      console.log('Web Socket Connection made!');
-    });
-    
-    ros.on('close', function() {
-      console.log('Web Socket Connection closed.');
-    });
-    
-    // Create a connection to the rosbridge WebSocket server.
-    ros.connect('ws://10.1.30.172:9090');
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
-    var panOn = false; 
-    var zoomStatus = false;
-    $(window).resize(function() {
-        clearTimeout(window.resizedFinished);
-        window.resizedFinished = setTimeout(function(){
-            windowWidth = window.innerWidth;
-            windowHeight = window.innerHeight;
-            console.log("Resized finished: width:"+windowWidth+"  height:"+windowHeight )
-            window.location.reload();    
-        }, 250);
-    });
 
 function init(){
 
