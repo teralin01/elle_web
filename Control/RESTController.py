c281ef08cdbd1d953532b8bf90cd73d4771ab4aa
Tera.Lin <Tera.Lin@axiomtek.com.tw>
2022-10-31 14:56:23 +0800

    Change internal function call for return data

--- Control/RESTController.py
+++ Control/RESTController.py
@@ -54,69 +54,73 @@ class RESTHandler(tornado.web.RequestHandler):
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET,PUT,DELETE, OPTIONS')
 
-    async def ROS_service_handler(self,calldata):
-        try:                    
-            await asyncio.wait_for( ROSConn.prepare_serviceCall_to_ROS(ROSConn,self.future,self.URI,calldata) , timeout = restTimeoutPeriod)
-            self.REST_response(self.future.result())
-
-        except asyncio.TimeoutError:
-            self._status_code = 500
-            self.REST_response(TimeoutStr)
-            #TODO trigger rosbrodge process         
-    async def ROS_service_handler_with_return(self,calldata):
+    async def ROS_service_handler(self,calldata,needReturn):
         serviceFuture = Future() 
         try:                    
             await asyncio.wait_for( ROSConn.prepare_serviceCall_to_ROS(ROSConn,serviceFuture,self.URI,calldata) , timeout = restTimeoutPeriod)
-            data = serviceFuture.result()
-            if data['result'] == True:
-                return True
+            if not needReturn:
+                self.REST_response(serviceFuture.result())
             else:
-                return False
+                data = serviceFuture.result()
+                if data['result'] == True:
+                    return True
+                else:
+                    return False            
 
         except asyncio.TimeoutError:
             self._status_code = 500
-            return False
+            if not needReturn:
+                self.REST_response(TimeoutStr)
+            else:
+                return False
 
-    async def ROS_subscribe_call_handler(self,calldata):
+    async def ROS_subscribe_call_handler(self,calldata,needReturn):
+        subFuture = Future()
         subdata = cacheSub.get(calldata['topic'])
         if  subdata != None and subdata['data'] != None:
             self.REST_response(subdata['data'])
         else:
             try:                    
-                await asyncio.wait_for( ROSConn.prepare_subscribe_from_ROS(ROSConn,self.future,calldata) , timeout = restTimeoutPeriod)
-                data = self.future.result()
-                if data != None:
-                    self.REST_response(data)
+                await asyncio.wait_for( ROSConn.prepare_subscribe_from_ROS(ROSConn,subFuture,calldata) , timeout = restTimeoutPeriod)
+                data = subFuture.result()
+                if not needReturn:
+                    if data != None:
+                        self.REST_response(data)
+                    else:
+                        self._status_code = 204 # No content 
+                        self.REST_response("result:False")
                 else:
-                    self._status_code = 204 # No content 
-                    self.REST_response("result:False")
-
+                        if data['result'] == True:
+                            return True
+                        else:
+                            return False  
+                    
             except asyncio.TimeoutError:
                 self._status_code = 500
-                self.REST_response(TimeoutStr)
+                if not needReturn:
+                    self.REST_response(TimeoutStr)
+                else:
+                    return False     
 
-    async def ROS_publish_handler(self,calldata):
-        try:                    
-            await asyncio.wait_for( ROSConn.prepare_publish_to_ROS(ROSConn,self.future,self.URI,calldata) , timeout = restTimeoutPeriod)
-            self.REST_response(self.future.result())
-
-        except asyncio.TimeoutError:
-            self._status_code = 500
-            self.REST_response(TimeoutStr)
-
-    async def ROS_publish_handler_with_return(self,calldata):
-        pubFuture = Future() 
+    async def ROS_publish_handler(self,calldata,needReturn):
+        pubFuture = Future()
         try:                    
             await asyncio.wait_for( ROSConn.prepare_publish_to_ROS(ROSConn,pubFuture,self.URI,calldata) , timeout = restTimeoutPeriod)
-            data = pubFuture.result()
-            if data['result'] == True:
-                return True
+            if not needReturn:
+                self.REST_response(pubFuture.result())
             else:
-                return False
+                data = pubFuture.result()
+                if data['result'] == True:
+                    return True
+                else:
+                    return False  
 
         except asyncio.TimeoutError:
             self._status_code = 500
-            return False
+            if not needReturn:
+                self.REST_response(TimeoutStr)
+            else:
+                return False
 
     def REST_response(self,data):
         # Todo add log if necessary
@@ -134,43 +138,43 @@ class RESTHandler(tornado.web.RequestHandler):
         
         elif self.URI == '/1.0/missions' or self.URI == '/1.0/missions/':
             subscribeMsg = {"op":"subscribe","id":"RestTopics","topic": "/mission_control/states","type":"elle_interfaces/msg/MissionControlMissionArray"}
-            await self.ROS_subscribe_call_handler(subscribeMsg)
+            await self.ROS_subscribe_call_handler(subscribeMsg,False)
             
         elif self.URI == '/1.0/maps' or self.URI == '/1.0/maps/GetMap':
             callData = {'id':self.URI, 'op':"call_service",'type': "nav_msgs/GetMap",'service': "/map_server/map",'args': {} }
-            await self.ROS_service_handler(callData)  
+            await self.ROS_service_handler(callData,False)  
                         
         elif self.URI == '/1.0/maps/map_meta' :  #TODO add map ID if support multiple maps
             subscribeMsg = {"op":"subscribe","id":"RestTopics","topic": "/amcl_pose","type":"geometry_msgs/msg/PoseWithCovarianceStamped"}
-            await self.ROS_subscribe_call_handler(subscribeMsg)
+            await self.ROS_subscribe_call_handler(subscribeMsg,False)
             
         elif self.URI == '/1.0/maps/speed_maps' :
             callData = {'id':self.URI, 'op':"call_service",'type': "nav_msgs/GetMap",'service': "/speed_filter_mask_server/map",'args': {} }
-            await self.ROS_service_handler(callData)      
+            await self.ROS_service_handler(callData,False)      
             
         elif self.URI == '/1.0/maps/keepout_maps' :
             callData = {'id':self.URI, 'op':"call_service",'type': "nav_msgs/GetMap",'service': "/keepout_filter_mask_server/map",'args': {} }
-            await self.ROS_service_handler(callData)                         
+            await self.ROS_service_handler(callData,False)                         
             
         elif self.URI == '/1.0/maps/path':       #TODO add robotID to the URL in fleet version
             subscribeMsg = {"op":"subscribe","id":"RestTopics","topic": "/plan","type":"nav_msgs/Path"}
-            await self.ROS_subscribe_call_handler(subscribeMsg)
+            await self.ROS_subscribe_call_handler(subscribeMsg,False)
             
         elif self.URI == '/1.0/maps/costmap':    #TODO add robotID to the URL in fleet version
             callData = {'id':self.URI, 'op':"call_service",'type': "nav2_msgs/srv/GetCostmap",'service': "/global_costmap/get_costmap",'args': {} }
-            await self.ROS_service_handler(callData)  
+            await self.ROS_service_handler(callData,False)  
             
         elif self.URI == '/1.0/ros/service':    
             callData = {'id':self.URI, 'op':"call_service",'type': self.get_argument("ros_type"),'service': self.get_argument("ros_service") }
-            await self.ROS_service_handler(callData)              
+            await self.ROS_service_handler(callData,False)              
             
         elif self.URI == '/1.0/ros/service/withArgs':    
             callData = {'id':self.URI, 'op':"call_service",'type': self.get_argument("ros_type"),'service': self.get_argument("ros_service"),'args': {self.get_argument("ros_args")} }
-            await self.ROS_service_handler(callData)              
+            await self.ROS_service_handler(callData,False)              
                         
         elif self.URI == '/1.0/ros/subscribe':       
             subscribeMsg = {"op":"subscribe","id":"RestTopics","topic":self.get_argument("ros_topic"),"type":self.get_argument("ros_type")}
-            await self.ROS_subscribe_call_handler(subscribeMsg)            
+            await self.ROS_subscribe_call_handler(subscribeMsg,False)            
             
         elif self.URI == '/1.0/status/hardware':    #TODO add robotID to the URL in fleet version
             self.REST_response(HWInfo.get())
@@ -217,11 +221,11 @@ class RESTHandler(tornado.web.RequestHandler):
                 return
             
             callData = {'type': "elle_interfaces/msg/MissionControlMission",'topic': "/mission_control/mission",'msg':data['mission']}
-            await self.ROS_publish_handler(callData)
+            await self.ROS_publish_handler(callData,False)
 
         elif self.URI == '/1.0/nav/initialpose': 
             publishMsg = {"op":"publish","id":"RestTopics","topic":"/initialpose","type":"geometry_msgs/msg/PoseWithCovarianceStamped",'msg':data['msg']}
-            await self.ROS_publish_handler(publishMsg)  
+            await self.ROS_publish_handler(publishMsg,False)  
 
         elif self.URI == '/1.0/nav/goalpose': 
             try:
@@ -235,9 +239,9 @@ class RESTHandler(tornado.web.RequestHandler):
                         
             publishMsg = {'type': "elle_interfaces/msg/MissionControlMission",'topic': "/mission_control/mission",'msg':data['mission']}
             callData = {'id':self.URI, 'op':"call_service",'type': "elle_interfaces/srv/MissionControlCmd",'service': "/mission_control/command",'args': {'command':0} }
-            ret = await self.ROS_publish_handler_with_return(publishMsg)
+            ret = await self.ROS_publish_handler(publishMsg,True)
             if ret == True:    
-                await self.ROS_service_handler(callData)
+                await self.ROS_service_handler(callData,False)
             else:
                 self.REST_response({'result':False})
             
@@ -245,11 +249,11 @@ class RESTHandler(tornado.web.RequestHandler):
             #example: {linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z:0}}"
             callData = {'type': "geometry_msgs/msg/Twist",'topic': "/cmd_vel",'msg':{
                 'linear':{ 'x':data['forwardspeed'],'y':0,'z':0 },'angular':{'x':0,'y':0,'z':data['turn']}  }}
-            await self.ROS_publish_handler(callData)                       
+            await self.ROS_publish_handler(callData,False)                       
             
         elif self.URI == '/1.0/ros/publish': 
             publishMsg = {"op":"publish","id":"RestTopics","topic":data['ros_topic'],"type":data['ros_type'],'msg':data['msg']}
-            await self.ROS_publish_handler(publishMsg)  
+            await self.ROS_publish_handler(publishMsg,False)  
 
         elif self.URI == '/1.0/maps/landmarks':
             #TODO validate with landmark Schema 
@@ -319,7 +323,7 @@ class RESTHandler(tornado.web.RequestHandler):
             self.REST_response({'result':False})
         elif self.URI == '/1.0/missions' or self.URI == '/1.0/missions/':  #start/stop mission
             callData = {'id':self.URI, 'op':"call_service",'type': "elle_interfaces/srv/MissionControlCmd",'service': "/mission_control/command",'args': {'command':data['command']} }
-            await self.ROS_service_handler(callData)        
+            await self.ROS_service_handler(callData,False)        
         elif self.URI == '/1.0/maps/landmarks':
             #TODO validate with landmark Schema 
             ret = LM.InsertPoint(data)
